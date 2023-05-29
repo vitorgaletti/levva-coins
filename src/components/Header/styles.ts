27d99f012b47e8fe87738d7be4149e5b1740cb5c
Vitor <vitor.fgaletti@gmail.com>
2023-05-29 09:39:00 -0300

    refactor: Modificação avatar do usuario;

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -73,16 +73,10 @@ interface UserAvatarProps {
 export const UserAvatar = styled.img<UserAvatarProps>`
   width: 3.1rem;
   height: 100%;
-  right: -6rem;
 
   border-radius: 50%;
   border: 2px solid transparent;
   box-shadow: 0px 0px 10px 10px #00000010;
-
-  @media only screen and (min-width: 1440px) {
-    position: absolute;
-    height: auto;
-  }
 
   transition: all 0.3;
 
@@ -93,10 +87,17 @@ export const UserAvatar = styled.img<UserAvatarProps>`
   }
 
   ${props =>
-    props.variant === 'large' &&
-    css`
-      width: 8rem;
-      height: 8rem;
-      margin-bottom: 1rem;
-    `}
+    props.variant === 'large'
+      ? css`
+          width: 8rem;
+          height: 8rem;
+          margin-bottom: 1rem;
+        `
+      : css`
+          @media only screen and (min-width: 1440px) {
+            position: absolute;
+            height: auto;
+            right: -6rem;
+          }
+        `}
 `;
